a65d714ecea485b53e8bcc90cd4b66925a3a3e1e
Pedro Martin <pataruco@gmail.com>
2018-05-24 09:31:01 +0100

    Working on style

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './style.css';
+
+const Header = () => (
+  <Fragment>
+    <picture>
+      <img src="" alt="General Assembly logo" />
+    </picture>
+    <h1>Technologies we learnt</h1>
+  </Fragment>
+);
+
+export default Header;
