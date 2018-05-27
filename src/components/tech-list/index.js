bfb38cf14a99a357e22589dab819334ba34eaa6c
Pedro Martin <pataruco@gmail.com>
2018-05-28 00:19:49 +0100

    animate list

--- src/components/tech-list/index.js
+++ src/components/tech-list/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import DeleteIcon from '../delete-icon';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
 import './style.css';
 
 const TechList = props => {
   const { deleteTechnology, technologies } = props;
+  let toDelete = false;
 
   const removeItem = item => {
+    toDelete = true;
     deleteTechnology(item);
   };
 
   const listTechnologies = technologies.map((technology, index) => {
     const key = index;
     return (
-      <li key={key} className="item">
+      <li key={key} className="item animated slideInDown">
         {technology}
         <button onClick={() => removeItem(technology)} className="button">
           <DeleteIcon />
@@ -21,7 +24,7 @@ const TechList = props => {
     );
   });
 
-  return <ul className="list">{listTechnologies}</ul>;
+  return <ol className="list">{listTechnologies}</ol>;
 };
 
 export default TechList;
